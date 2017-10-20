ede303855a7c15dd675a53b69b256fa983cf1ecc
siscely <siscely.george@gmail.com>
2017-10-20 18:22:00 +0000

    commit changes

---
+++ models/address_book.rb
@@ -0,0 +1,32 @@
+require_relative 'entry'
+require_relative '../models/address_book'
+
+class AddressBook
+    attr_reader :entries
+    
+     def initialize
+     @entries = []
+     end
+ 
+ def add_entry(name, phone_number, email)
+     
+     index = 0
+     entries.each do |entry|
+         
+         if name < entry.name
+         break
+       end
+       index+= 1
+     end
+     
+     entries.insert(index, Entry.new(name, phone_number, email))
+   end
+   
+  def remove_entry(name, phone_number, email)
+     entries.each_with_index do |index, entry|
+         if name == entry.name
+             entries.delete_at(index)
+         end
+    end
+end
+end
