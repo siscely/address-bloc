e59e6c6c501638b8e6f9099eeaa31fc72d938da2
siscely <siscely.george@gmail.com>
2017-10-23 20:39:02 +0000

    commit changes

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
 require_relative 'entry'
 require_relative '../models/address_book'
+require "csv"
 
 class AddressBook
     attr_reader :entries
@@ -8,25 +9,66 @@ class AddressBook
      @entries = []
      end
  
- def add_entry(name, phone_number, email)
+    def add_entry(name, phone_number, email)
      
      index = 0
      entries.each do |entry|
          
          if name < entry.name
          break
-       end
-       index+= 1
-     end
+         end
+        index+= 1
+        end
      
      entries.insert(index, Entry.new(name, phone_number, email))
+    end
+
+      def remove_entry(name, phone_number, email)
+         entries.each_with_index do |entry, index|
+             if name == entry.name
+                 entries.delete_at(index)
+             end
+        end
+    end
+    
+        def import_from_csv(file_name)
+         csv_text = File.read(file_name)
+         csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+       
+         csv.each do |row|
+           row_hash = row.to_hash
+           add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+         end
+       end
+       
+   def binary_search(name)
+       lower = 0
+     upper = entries.length - 1
+ 
+      while lower <= upper
+     
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+     
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+ 
+     
+     return nil
    end
    
-  def remove_entry(name, phone_number, email)
-     entries.each_with_index do |entry, index|
+   def iterative_search(name)
+     entries.each do |entry|
          if name == entry.name
-             entries.delete_at(index)
-         end
+             return entries.name
+         else
+             return nil
+        end
     end
-end
-end
