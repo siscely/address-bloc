42f91cb4df0003f3da1863727fd8ddbc9d6b16c0
siscely <siscely.george@gmail.com>
2017-10-24 17:45:02 +0000

    commit changes

--- models/address_book.rb
+++ models/address_book.rb
@@ -11,34 +11,34 @@ class AddressBook
  
     def add_entry(name, phone_number, email)
      
-     index = 0
-     entries.each do |entry|
+        index = 0
+        entries.each do |entry|
          
          if name < entry.name
          break
          end
         index+= 1
-     end
+        end
      
-     entries.insert(index, Entry.new(name, phone_number, email))
+        entries.insert(index, Entry.new(name, phone_number, email))
     end
 
-     def remove_entry(name, phone_number, email)
+    def remove_entry(name, phone_number, email)
          entries.each_with_index do |entry, index|
              if name == entry.name
                  entries.delete_at(index)
              end
-     end
+    end
     
-     def import_from_csv(file_name)
-         csv_text = File.read(file_name)
-         csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    def import_from_csv(file_name)
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
        
-         csv.each do |row|
-           row_hash = row.to_hash
-           add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-         end
-     end
+        csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
+    end
        
     def binary_search(name)
        lower = 0
@@ -50,23 +50,24 @@ class AddressBook
        mid_name = entries[mid].name
  
      
-       if name == mid_name
+         if name == mid_name
          return entries[mid]
-       elsif name < mid_name
+         elsif name < mid_name
          upper = mid - 1
-       elsif name > mid_name
+         elsif name > mid_name
          lower = mid + 1
-       end
-     end
+         end
+        end
     return nil
    end
   
     def iterative_search(name)
      entries.each do |entry|
-         if name == entry.name
-             return entries.name
-         else
-             return nil
+        if name == entry.name
+         return entries.name
+        else
+         return nil
         end
+     end
     end
 end
