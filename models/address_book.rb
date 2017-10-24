3d44fecdeca59ceaf711ed29b769698e554ccf75
siscely <siscely.george@gmail.com>
2017-10-24 19:54:01 +0000

    commit changes

--- models/address_book.rb
+++ models/address_book.rb
@@ -5,14 +5,14 @@ require "csv"
 class AddressBook
     attr_reader :entries
     
-     def initialize
+    def initialize
      @entries = []
-     end
+    end
  
     def add_entry(name, phone_number, email)
      
-     index = 0
-     entries.each do |entry|
+        index = 0
+        entries.each do |entry|
          
          if name < entry.name
          break
@@ -20,55 +20,55 @@ class AddressBook
         index+= 1
         end
      
-     entries.insert(index, Entry.new(name, phone_number, email))
+        entries.insert(index, Entry.new(name, phone_number, email))
     end
 
-      def remove_entry(name, phone_number, email)
+    def remove_entry(name, phone_number, email)
          entries.each_with_index do |entry, index|
              if name == entry.name
                  entries.delete_at(index)
              end
-        end
-    
-        def import_from_csv(file_name)
-         csv_text = File.read(file_name)
-         csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-       
-         csv.each do |row|
-           row_hash = row.to_hash
-           add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
          end
-       end
+    end
+    
+    def import_from_csv(file_name)
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
        
-   def binary_search(name)
+        csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
+    end
+       
+    def binary_search(name)
        lower = 0
-     upper = entries.length - 1
+       upper = entries.length - 1
  
-      while lower <= upper
+       while lower <= upper
      
        mid = (lower + upper) / 2
        mid_name = entries[mid].name
  
      
-       if name == mid_name
+         if name == mid_name
          return entries[mid]
-       elsif name < mid_name
+         elsif name < mid_name
          upper = mid - 1
-       elsif name > mid_name
+         elsif name > mid_name
          lower = mid + 1
-       end
-     end
- 
-     
-     return nil
-   end
-   
-   def iterative_search(name)
-     entries.each do |entry|
-         if name == entry.name
-             return entries.name
-         else
-             return nil
+         end
         end
+    return nil
+   end
+  
+    def iterative_search(name)
+     entries.each do |entry|
+        if entry.name == name
+         return entry
+        else
+         return nil
+        end
+     end
     end
 end
